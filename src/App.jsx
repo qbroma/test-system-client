4b4aeb0870f67d8080258510d6b9895862b0c842
Raman Valasevich <raman.valasevich@gmail.com>
2023-07-05 20:13:26 +0300

    Start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material';
+
+export const App = () => {
+    return (
+        <Box
+            sx={{
+                width: 100,
+                height: 100,
+            }}
+        />
+    );
+}``
